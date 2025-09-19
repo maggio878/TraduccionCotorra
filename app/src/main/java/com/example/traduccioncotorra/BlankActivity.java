e171c5fffc564f3f719c4aaf919ea81d16c46e4d
maggio878 <josemarioluquefernandez@gmail.com>
2025-09-18 19:41:48 -0700

    17/09/25

--- app/src/main/java/com/example/traduccioncotorra/BlankActivity.java
+++ app/src/main/java/com/example/traduccioncotorra/BlankActivity.java
@@ -8,35 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class BlankActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_blank);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button loginButton = findViewById(R.id.btn_login);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Crear un Intent para iniciar la nueva actividad (BlankActivity)
-                Intent intent = new Intent(MainActivity.this, BlankActivity.class);
-                startActivity(intent);
-            }
-        });
     }
-
-
 }
